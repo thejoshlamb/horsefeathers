2266ac01c91e0d5fe9f25715320fe22e38b18f55
Josh Lamb <joshlamb@gmail.com>
2020-09-15 20:40:50 -0700

    changed name of socket event

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (request, response) => {
-  response.sendFile('/index.html'{ root: __dirname }).status(200);
+  response.sendFile('/index.html', { root: __dirname });
 });
 
 router.get("/new-room", (request, response) => {
