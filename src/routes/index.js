877f2a6e22afa8bb876e9996fb22e76398019a34
Josh Lamb <joshlamb@gmail.com>
2020-09-15 20:37:47 -0700

    learning to save in atom

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (request, response) => {
-  response.send({ response: "this is the root" }).status(200);
+  response.sendFile('/index.html'{ root: __dirname }).status(200);
 });
 
 router.get("/new-room", (request, response) => {
